1481933e9d35f309a229776b3540d0c91c7f3eae
daniraVazquezC <danira.vazquez2811@gmail.com>
2021-03-27 19:30:55 -0600

    create avatar model

---
+++ db/migrate/20210328012923_create_avatars.rb
@@ -0,0 +1,10 @@
+class CreateAvatars < ActiveRecord::Migration[6.1]
+  def change
+    create_table :avatars do |t|
+      t.string :name
+      t.string :description
+      t.string :image
+      t.timestamps
+    end
+  end
+end
